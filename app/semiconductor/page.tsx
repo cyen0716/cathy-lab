5a3b08a7f3dfa01549e531de377886e665f677b2
Cathy <91357231+cyen0716@users.noreply.github.com>
2026-05-29 22:54:22 +0800

    Update page.tsx

--- app/semiconductor/page.tsx
+++ app/semiconductor/page.tsx
@@ -188,7 +188,7 @@ export default function SemiconductorPage() {
     <div className="text-center">
 
   <div className="bg-[#EEF2FF] border border-[#C7D2FE] rounded-xl px-10 py-4">
-    <p className="text-xl text-slate-900">
+    <p className="text-base text-slate-900">
       ON
     </p>
   </div>
