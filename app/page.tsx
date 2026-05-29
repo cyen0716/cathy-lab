1fee177402421de45c5ead2a0291e0a88298a5ab
Cathy <91357231+cyen0716@users.noreply.github.com>
2026-05-29 14:02:46 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -71,7 +71,7 @@ export default function HomePage() {
                 <div>
 
                   <div className="text-4xl mb-3">
-                    🇰🇷
+                    📗
                   </div>
 
                   <h3 className="text-2xl font-bold text-slate-900">
