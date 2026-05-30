3a6482c1780fc3b32dd2b499e8d9270a867f6440
Cathy <91357231+cyen0716@users.noreply.github.com>
2026-05-31 03:08:36 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function HomePage() {
         </nav>
 
         {/* Hero Section */}
-        <section className="max-w-4xl mb-14">
+        <section className="max-w-6xl mb-14">
 
           <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-slate-900 tracking-tight mb-8">
             Hi, I’m <span className="text-[#4338CA] bg-gradient-to-r from-[#4338CA] to-[#6366F1] bg-clip-text text-transparent">Cathy</span>
